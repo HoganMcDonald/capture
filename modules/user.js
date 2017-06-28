7dd07f7c26d6609ece0f613ae71b739aea501751
Hogan McDonald <hpeter.mcdonald@icloud.com>
2017-06-28 16:02:43 -0500

    updated heroku uri

--- modules/user.js
+++ modules/user.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('localhost:27017/Capture');
+mongoose.connect('mongodb://heroku_g2fn8fff:1r4653fee7b45j2otfuf7lfd6g@ds141082.mlab.com:41082/heroku_g2fn8fff');
 
 var userSchema = new mongoose.Schema({
   username: String,
